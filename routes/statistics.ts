89ff1fce6b06015c9a3a724ab6b8da20298876e6
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2021-01-21 22:28:37 +0300

    feat: add swagger

--- routes/statistics.ts
+++ routes/statistics.ts
@@ -1,36 +1,49 @@
-import express from 'express';
-const router = express.Router();
 import { GameModel } from '../types/game';
+import { BodyProp, Controller, Delete, Get, Post, Route, Tags } from 'tsoa';
 
-router.route('/').get((req, res) => {
-  GameModel.find({})
-    .then((items: any) => res.json(items))
-    .catch((err: any) => res.status(400).json('Error: ' + err));
-});
+@Route('/statistics')
+@Tags('StatisticsController')
+export class StatisticsController extends Controller {
+  @Get()
+  public async getAll(): Promise<any[]> {
+    return GameModel.find({})
+      .then((items: any) => items)
+      .catch((err: any) => this.setStatus(500));
+  }
 
-router.route('/:id').get((req, res) => {
-  GameModel.findById(req.params.id)
-    .then((game) => res.json(game))
-    .catch((err) => res.status(400).json('Error: ' + err));
-});
+  @Post()
+  public async create(
+    @BodyProp() userId: string,
+    @BodyProp() score: string,
+    @BodyProp() totalTime: string
+  ): Promise<any> {
+    const newGame = new GameModel({
+      userId: userId,
+      score: score,
+      totalTime: totalTime,
+    });
+    this.setStatus(201);
 
-router.route('/').post((req, res) => {
-  const newGame = new GameModel({
-    userId: req.body.userId,
-    score: req.body.score,
-    totalTime: req.body.totalTime,
-  });
+    return newGame
+      .save()
+      .then((item) => {
+        this.setStatus(201);
+        return item;
+      })
+      .catch((err) => this.setStatus(500));
+  }
 
-  newGame
-    .save()
-    .then((item) => res.json(item))
-    .catch((err) => res.status(400).json('Error: ' + err));
-});
+  @Get('/{userId}')
+  public async getByUserId(userId: string): Promise<any[]> {
+    return GameModel.find({ userId: userId })
+      .then((items: any) => items)
+      .catch((err: any) => this.setStatus(500));
+  }
 
-router.route('/:userId').delete((req, res) => {
-  GameModel.deleteMany({ userId: req.params.userId })
-    .then(() => res.json('Game deleted.'))
-    .catch((err) => res.status(400).json('Error: ' + err));
-});
-
-export default router;
+  @Delete('/{userId}')
+  public async deleteByUserId(userId: string): Promise<void> {
+    GameModel.deleteMany({ userId: userId })
+      .then(() => this.setStatus(204))
+      .catch((err) => this.setStatus(500));
+  }
+}
